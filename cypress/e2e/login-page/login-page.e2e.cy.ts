ce75463026a3bd25dae287de88c10b11bcd318d2
Faizan <faizanshk290@gmail.com>
2025-11-16 12:54:38 +0530

    Added First Commit

---
+++ cypress/e2e/login-page/login-page.e2e.cy.ts
@@ -0,0 +1,13 @@
+import { generateTestCases, saveTestCasesToFile, type TestCases } from '../../support/test-data-generator';
+
+const testCases: TestCases = generateTestCases(5);
+
+describe('Login Page Tests', () => {
+  before(() => {
+    saveTestCasesToFile(testCases, 'cypress/fixtures/generated-test-cases.json');
+  });
+
+  it(testCases['TEAM-12300'].description, () => {
+    expect(true).to.equal(true);
+  });
+});
